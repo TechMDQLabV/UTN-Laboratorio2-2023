108a1aeddb82b4a83592e6068482cb3899cd86b1
Sergio Garguir <sergiogarguir@gmail.com>
2023-08-23 19:37:12 -0300

    mas recursividad

--- 2023-08-22 - recursividad/main.c
+++ 2023-08-22 - recursividad/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int arreglo[]={1,2,3,4,5,6,7,8};
+    int arreglo[]={1,2,3,5,6,7,8,9};
     int vArreglo = 8;
     printf("\n %d", factorial(5));
     printf("\n %d", factorial1(5));
@@ -15,6 +15,7 @@ int main()
     muestraArreglo(arreglo, vArreglo, 0);
     printf("\n");
     muestraArregloInvertido(arreglo, vArreglo, 0);
+
     return 0;
 }
 
@@ -73,14 +74,59 @@ void muestraArreglo(int a[], int v, int i){
 
 void muestraArregloInvertido(int a[], int v, int i){
     if(i < v){
-        muestraArreglo(a, v, i + 1);
+        muestraArregloInvertido(a, v, i + 1);
         printf("%d - ", a[i]);
     }
 }
 
+int capicua(int a[], int i, int u){
+    int rta;
+    if(i<u){
+        if(a[i]==a[u]){
+            rta=capicua(a, i+1, u-1);
+        }else{
+            rta=0;
+        }
+    }else{
+        rta = 1;
+    }
+    return rta;
+}
 
+int capicua(int a[], int i, int u){
+    int rta=1;
+    if(i<u){
+        if(a[i]==a[u]){
+            rta=capicua(a, i+1, u-1);
+        }else{
+            rta=0;
+        }
+    }
+    return rta;
+}
 
+int buscaMenorEnArreglo(int a[], int v, int i){
+    int menor;
+    if(i==v-1){
+        menor = a[i];
+    }else{
+        menor = buscaMenorEnArreglo(a, v, i+1);
+        if(a[i]<menor){
+            menor=a[i];
+        }
+    }
+    return menor;
+}
 
+int sumaArreglo(int a[], int v, int i){
+    int rta = 0;
+    if(i<v){
+        rta = a[i] + sumaArreglo(a, v, i+1);
+    }
+    return rta;
+}
 
-
+int sumaArregloTernario(int a[], int v, int i){
+    return (i<v) ? a[i] + sumaArregloTernario(a, v, i+1) : 0;
+}
 
