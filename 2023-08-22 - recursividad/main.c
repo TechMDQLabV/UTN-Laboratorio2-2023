b5a932d9183f77db10b2471ef96e736239ff818a
Sergio Garguir <sergiogarguir@gmail.com>
2023-08-22 21:33:18 -0300

    mas recursividad

--- 2023-08-22 - recursividad/main.c
+++ 2023-08-22 - recursividad/main.c
@@ -3,10 +3,18 @@
 
 int main()
 {
-
+    int arreglo[]={1,2,3,4,5,6,7,8};
+    int vArreglo = 8;
     printf("\n %d", factorial(5));
     printf("\n %d", factorial1(5));
     printf("\n %d", factorial2(5));
+    printf("\n %d", potencia(2, 6));
+    printf("\n %d", potencia1(2, 6));
+    printf("\n %d", potencia2(2, 6));
+    printf("\n");
+    muestraArreglo(arreglo, vArreglo, 0);
+    printf("\n");
+    muestraArregloInvertido(arreglo, vArreglo, 0);
     return 0;
 }
 
@@ -33,6 +41,46 @@ int factorial1(int x){
 int factorial2(int x){
     return (x > 0) ? x * factorial2(x-1) : 1;
 }
+
+int potencia(int base, int exponente){
+    int rta;
+    if(exponente == 0){
+        rta = 1;
+    }else{
+        rta = base * potencia(base, exponente - 1);
+    }
+    return rta;
+}
+
+int potencia1(int base, int exponente){
+    int rta = 1;
+    if(exponente > 0){
+        rta = base * potencia1(base, exponente - 1);
+    }
+    return rta;
+}
+
+int potencia2(int base, int exponente){
+    return (exponente > 0) ? base * potencia(base, exponente - 1) : 1;
+}
+
+void muestraArreglo(int a[], int v, int i){
+    if(i < v){
+        printf("%d - ", a[i]);
+        muestraArreglo(a, v, i + 1);
+    }
+}
+
+void muestraArregloInvertido(int a[], int v, int i){
+    if(i < v){
+        muestraArreglo(a, v, i + 1);
+        printf("%d - ", a[i]);
+    }
+}
+
+
+
+
 
 
 
